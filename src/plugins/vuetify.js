59b27988282a5276df3d110592c427107b03d8c2
Kevin Stewart <kevin-stewart1@hotmail.co.uk>
2021-01-16 14:31:47 +0000

    Style Landing Page
    Removed base src and move project up a layer
    
    Set background colour to white smoke

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.purple,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+        background: '#f5f5f5'
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+        background: colors.indigo.base
+      }
+    }
+  }
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <v-row>
+      <v-col md="6" sm="6" class="mx-auto auto">
+        <img contain class="auto" :src="require('../assets/logo-place-holder.png')" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
